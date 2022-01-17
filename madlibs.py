10a7b69cfa9121be8b61f1ba9ddcaa1723c9bda7
FIftekhar <iftekhar.f.19@gmail.com>
2022-01-17 14:03:02 -0500

    first commit

---
+++ madlibs.py
@@ -0,0 +1,12 @@
+# Derived from freecodecamp.org's Beginner Python tutorial video
+
+print("Welcome to Mad Libs!")
+
+color = input("Enter a color: ")
+plural_noun = input("Enter a plural noun: ")
+celebrity = input("Enter a celebrity you love: ")
+
+print("")
+print("Roses are " + color)
+print(plural_noun + " are blue")
+print("I love " + celebrity)
